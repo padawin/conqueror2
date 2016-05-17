172870669086d4427f023e3c25c45581ec1cdd2a
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-05-17 16:01:32 +0100

    graph generated and drawn

--- js/graph.js
+++ js/graph.js
@@ -9,14 +9,35 @@ function (canvas, B) {
 
 	var canvasContext = canvas.getContext();
 
+	function generate (graph, nbNodes) {
+		while (nbNodes > 0) {
+			graph.nodes.push({
+				x: Math.random() * 1000,
+				y: Math.random() * 1000
+			});
+			nbNodes--;
+		}
+	}
+
 	/**
 	 * Map construct. Build the level, set the objects and the frame information
 	 */
-	function Graph (nbNodes) {
-		var graph = {};
+	function Graph (nbNodes, surfaceW, surfaceH) {
+		var graph = {
+			nodes: [],
+			edges: []
+		};
+
+		generate(graph, nbNodes);
 
 		graph.draw = function (camera) {
-
+			canvasContext.beginPath();
+			graph.nodes.forEach(function (node) {
+				var coords = camera.adapt(node);
+				canvasContext.moveTo(coords.x, coords.y);
+				canvasContext.arc(coords.x, coords.y, 5, 0, 2 * Math.PI, false);
+			});
+			canvasContext.fill();
 		};
 
 		return graph;
