951ce86b114376e1d5b8b105139e575b4926246f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-06-07 13:06:21 +0100

    nodesGrid integrity checked

--- tests/server/testGraph.py
+++ tests/server/testGraph.py
@@ -19,3 +19,15 @@ class graphTests(tests.common.common):
 		)
 
 		self.assertEquals(len(g.nodes), nodesNumber)
+		nbNone = 0
+		nbNodes = 0
+		for row in g.nodesGrid:
+			for node in row:
+				if node is None:
+					nbNone += 1
+				else:
+					self.assertEquals(node, {'owned_by': None})
+					nbNodes += 1
+
+		self.assertEquals(nbNone, graphWidth * graphHeight - nodesNumber)
+		self.assertEquals(nbNodes, nodesNumber)
