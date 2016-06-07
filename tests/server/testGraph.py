5114b02946f112e65b285665a7df3b9d9c7ea971
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-06-07 13:06:06 +0100

    hard coded values removed

--- tests/server/testGraph.py
+++ tests/server/testGraph.py
@@ -7,7 +7,15 @@ import tests.common
 
 class graphTests(tests.common.common):
 	def test_nodes_generation(self):
+		graphWidth = 5
+		graphHeight = 5
+		nodesNumber = 5
 		g = graph.graph()
-		g.generateNodes(5, 5, 5, lambda: {'owned_by': None})
+		g.generateNodes(
+			nodesNumber,
+			graphWidth,
+			graphHeight,
+			lambda: {'owned_by': None}
+		)
 
-		self.assertEquals(len(g.nodes), 5)
+		self.assertEquals(len(g.nodes), nodesNumber)
